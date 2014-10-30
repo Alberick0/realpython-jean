ce3ada2c7e05a1281a2337c2cc54501702793eca
Jean Guzman <jean.guzman@ceroic.com>
2014-10-30 08:20:32 -0600

    modified temperature.py to use strings like auction buddy

---
+++ part1/chapter9/poetry.py
@@ -0,0 +1,21 @@
+import random
+
+# declaring lists
+nouns = ['fossil', 'horse', 'aardvark', 'judge', 'chef', 'mango', 'extrovert', 'gorilla']
+
+verbs = ['kicks', 'jingles', 'bounces', 'slurps', 'meows', 'explodes', 'curdles']
+
+prepositions = ['against', 'after', 'into', 'beneath', 'upon', 'for', 'in', 'like', 'over', 'within']
+
+adverbs = ['curiously', 'extravagantly', 'tantalizingly', 'furiously', 'sensuously']
+
+# main function of the script
+def makePoem():
+    poem = ''' '''
+    
+    return poem
+
+
+makePoem()
+
+
