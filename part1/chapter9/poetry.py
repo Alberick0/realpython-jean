d7c03847fb02a66a7ed00510ce79fa859b2f6657
Jean Guzman <jean.guzman@ceroic.com>
2014-10-30 14:11:28 -0600

    added poetry assignment

--- part1/chapter9/poetry.py
+++ part1/chapter9/poetry.py
@@ -1,21 +1,41 @@
 import random
 
-# declaring lists
-nouns = ['fossil', 'horse', 'aardvark', 'judge', 'chef', 'mango', 'extrovert', 'gorilla']
+# 0 = nouns | 1 = verbs | 2 = adjectives | 3 = prepositions | 4 = Adverbs
+types = [
+    ['fossil', 'horse', 'aardvark', 'judge', 'chef', 'mango', 'extrovert', 'gorilla'],
+    ['kicks', 'jingles', 'bounces', 'slurps', 'meows', 'explodes', 'curdles'],
+    ['furry', 'balding', 'incredulous', 'fragant', 'exuberant', 'glistening'],
+    ['against', 'after', 'into', 'beneath', 'upon', 'for', 'in', 'like', 'over', 'within'],
+    ['curiously', 'extravagantly', 'tantalizingly', 'furiously', 'sensuously']
+]
 
-verbs = ['kicks', 'jingles', 'bounces', 'slurps', 'meows', 'explodes', 'curdles']
+poem = [ [], [], [], [], [] ]
 
-prepositions = ['against', 'after', 'into', 'beneath', 'upon', 'for', 'in', 'like', 'over', 'within']
+def adder(i,amount):
+    while len(poem[i]) < amount:
+       word = random.choice(types[i])
 
-adverbs = ['curiously', 'extravagantly', 'tantalizingly', 'furiously', 'sensuously']
+       if poem[i].count(word) == 0:
+           poem[i].append(word)
 
-# main function of the script
+
 def makePoem():
-    poem = ''' '''
-    
-    return poem
+    for i in range(len(types)):
+        if i < 3:
+            adder(i, 3)
+        elif i == 3:
+            adder(i, 2)
+        else:
+            adder(i, 1)
 
+    # 0 = nouns | 1 = verbs | 2 = adjectives | 3 = prepositions | 4 = Adverbs
+    return '''
+A {0[2][0]} {0[0][0]}
 
-makePoem()
+A {0[2][0]} {0[0][0]} {0[1][0]} {0[3][0]} the {0[2][1]} {0[0][1]}
+{0[4][0]}, the {0[0][0]} {0[1][1]}
+the {0[0][1]} {0[1][2]} {0[3][1]} a {0[2][1]} {0[0][2]}
+'''.format(poem)
 
+print makePoem()
 
