3830ccb7cd02d5439bd4180bd7843783b313318f
Jean Guzman <jean.guzman@ceroic.com>
2014-10-30 15:37:46 -0600

    fixed coin toss assignment

--- part1/chapter8/coin_toss.py
+++ part1/chapter8/coin_toss.py
@@ -1,31 +1,19 @@
+from __future__ import division
 from random import randint
 
-trial = 10
-toss_tail = 0
-toss_head = 0
-head = 0
-tail = 1
+trial = 10000
+flips = 0
 
-# head = 0 tail = 1
+# Heads = 0 | Tail = 1
+for i in range(trial):
+    flips += 1
 
-if randint(0,1) == 0:
-    head += 1
+    if randint(0,1) == 0:
+        flips += 1
+        while True:
+            flips += 1
+            if randint(0,1) == 1:
+                flips += 1
+                break
 
-    while(tail == 0):
-        toss_tail += 1
-
-        if randint(0,1) == 1:
-            tail += 1
-
-    print "The average tosses for tail were", toss_tail
-
-else:
-    tail += 1
-
-    while(head == 0):
-        toss_head += 1
-
-        if randint(0,1) == 0:
-            head += 1
-
-    print "The average tosses for head were", toss_head
+print flips/trial
