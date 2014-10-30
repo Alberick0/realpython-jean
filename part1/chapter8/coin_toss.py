cdde6854c1125454767c823259d8c1c3b2181116
Jean Guzman <jean.guzman@ceroic.com>
2014-10-30 15:45:24 -0600

    rebuilt coin toss

--- part1/chapter8/coin_toss.py
+++ part1/chapter8/coin_toss.py
@@ -9,11 +9,12 @@ for i in range(trial):
     flips += 1
 
     if randint(0,1) == 0:
-        flips += 1
-        while True:
+        while randint(0,1) == 0:
             flips += 1
-            if randint(0,1) == 1:
-                flips += 1
-                break
+        flips += 1
+    else:
+        while randint(0,1) == 1:
+            flips += 1
+        flips += 1
 
 print flips/trial
