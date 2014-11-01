b703cfb2a0b2ee840ebc3f4c4f636b2c1c9ea506
Jean Guzman <jean.guzman@ceroic.com>
2014-11-01 10:07:25 -0600

    completed cats with hats and started next assignment

---
+++ part1/chapter9/fundamental_review.py
@@ -0,0 +1,21 @@
+print "\n# -- part 1 -- #"
+
+# Modify the variable value so taht all of the 
+# 'print' statements return 'True'
+
+zero = 1
+one = 2
+two = [5, 4, 3, 2, 1, 0]
+three = 'I love Python!'
+four = [['P', 'y', 't', 'h', 'o', 'n'], ['i', 's'], ['h', 'a', 'r', 'd']]
+five = {'happy':'birthday'}
+six = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+days = ('Fri', 'Sat', 'Sun')
+x, y, seven = days
+
+# DO NOT CHANGE ANYTHING BELOW THIS LINE #
+# -------------------------------------- #
+
+print 'zero: {}'.format(zero == 0)
+print 'one:  {}'.format(one > 22)
+print 'two:  {}'.format(len(two) == 5)
