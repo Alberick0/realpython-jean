7cd03a87dc02f296cc11497aa346763e21e5eab0
Jean Guzman <jean.guzman@ceroic.com>
2014-12-09 15:36:41 -0600

    test tweaks

--- part1/chapter2/3ex_script.py
+++ part1/chapter2/3ex_script.py
@@ -1,8 +1,8 @@
-'''1. Using the interactive window, display some text on the screen by using print
+"""1. Using the interactive window, display some text on the screen by using print
 2. Using the interactive window, display a string of text by saving the string to a variable,
 then printing the contents of that variable
 3. Do each of the first two exercises again by first saving your code in a script and then
-running the script'''
+running the script"""
 
 print "some text"
 var = "string saved in a variable"
