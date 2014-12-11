2f03e65d0570991e486712483067c45a455543c7
Jean Guzman <jean.guzman@ceroic.com>
2014-12-11 01:21:47 -0600

    updating branch with latest advances

---
+++ part2/chapter6/sql/homework-4/2.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+"""
+• Using the COUNT() function, calculate the total number of orders for each make and
+model.
+• Output the car’s make and model on one line, the quantity on another line, and then
+the order count on the next line. The latter is a bit difficult, but please try it first before
+looking at my answer. Remember: Google-it-first!
+"""
