6f211c899cc1ac05557d5f583cd573ffcebc6589
Jean Guzman <jean.guzman@ceroic.com>
2015-01-09 16:38:42 -0600

    chapter 16 advanvces

---
+++ part2/chapter16/flask-bdd/flaskr.py
@@ -0,0 +1,13 @@
+# importing Flask
+from flask import Flask
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run()
