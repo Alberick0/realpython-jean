0bb3ac028371500032c1787ab60346046f85f45a
Jean Guzman <jean.guzman@ceroic.com>
2015-01-11 00:32:08 -0600

    added a few thing to this chapter. need to keep working this.

--- part2/chapter25/intro-js-jquery/main.js
+++ part2/chapter25/intro-js-jquery/main.js
@@ -5,4 +5,12 @@
 
 $(function(){
     console.log("whee!")
+
+    //event handler
+    $("btn-click").click(function(){
+        if ($('input').val() !== '') {
+            var input = $('input').val()
+            console.log(input)
+        }
+    })
 });
