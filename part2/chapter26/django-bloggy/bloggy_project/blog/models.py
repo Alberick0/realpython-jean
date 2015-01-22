b83fcb6269d3ed1cb9a57a1bf2ec718bfdb5cc26
Jean Guzman <jean.guzman@ceroic.com>
2015-01-21 23:16:42 -0600

    Completed chapter 26, my django blog app

--- part2/chapter26/django-bloggy/bloggy_project/blog/models.py
+++ part2/chapter26/django-bloggy/bloggy_project/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from uuslug import uuslug
+
+
 class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
@@ -8,6 +11,11 @@ class Post(models.Model):
     tag = models.CharField(max_length=20, blank=True, null=True)
     image = models.ImageField(upload_to='images', blank=True, null=True)
     views = models.IntegerField(default=0)
+    slug = models.CharField(max_length=100, unique=True)
 
     def __unicode__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = uuslug(self.title, instance=self, max_length=100)
+        super(Post, self).save(*args, **kwargs)
--- part2/chapter26/django-bloggy/bloggy_project/blog/views.py
+++ part2/chapter26/django-bloggy/bloggy_project/blog/views.py
@@ -1,32 +1,34 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.template import Context, loader, RequestContext
 from blog.models import Post
 from blog.forms import PostForm
 
 # Create your views here.
-def encode_url(posts):
-    for post in posts:
-        post.url = post.title.replace(' ', '_')
-    return posts
+########################
+### helper functions ###
+########################
+def get_popular_posts():
+    return Post.objects.order_by('views')[:5]
 
-
+######################
+### view functions ###
+######################
 def index(request):
-    latest_posts = encode_url(Post.objects.all().order_by('-created_at'))
-    popular_posts = encode_url(Post.objects.all().order_by('-views')[:5])
+    latest_posts = Post.objects.all().order_by('-created_at')
     t = loader.get_template('blog/index.html')
-    context_dict = {'latest_posts': latest_posts, 'popular_posts': popular_posts}
+    context_dict = {'latest_posts': latest_posts, 'popular_posts': get_popular_posts()}
     c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 
 def post(request, slug):
-    single_post = get_object_or_404(Post, title=slug.replace('_', ' '))
-    popular_posts = encode_url(Post.objects.all().order_by('-views')[:5])
+    single_post = get_object_or_404(Post, slug=slug)
     single_post.views += 1  # used to increment the number of views
     single_post.save()  # saves the increment
     t = loader.get_template('blog/post.html')
-    c = Context({'single_post': single_post, 'popular_posts': popular_posts})
+    context_dict = {'single_post': single_post, 'popular_posts': get_popular_posts()}
+    c = Context(context_dict)
     return HttpResponse(t.render(c))
 
 
@@ -36,7 +38,9 @@ def add_post(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():  # this is going to check if the form values are valid
             form.save(commit=True)  # if valid save to DB
-            return post(request, form.cleaned_data['title'])
+            new_post = Post.objects.filter(title=form.cleaned_data['title']).order_by('-created_at')[:1]
+            return post(request, new_post[0].slug)
+            # return redirect(index)
         else:
             print form.errors  # displays the errors on the form
     else:
