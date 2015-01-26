150402c38512d9d018a43985f6b7bfea731ccda7
Jean Guzman <jean.guzman@ceroic.com>
2015-01-26 12:13:18 -0600

    updating chapter 25 with latest advances

--- part2/chapter25/intro-js-jquery/main.js
+++ part2/chapter25/intro-js-jquery/main.js
@@ -7,10 +7,16 @@ $(function(){
     console.log("whee!")
 
     //event handler
-    $("btn-click").click(function(){
+    $("#btn-click").click(function(){
         if ($('input').val() !== '') {
+            // grab the value from the input box after the click
             var input = $('input').val()
+            // display the value within the browser console
             console.log(input)
+            // add the value to the dom
+            $('ol').append('<li><a href="">x</a>' + input + '</li>');
         }
-    })
+        $('input').val('');
+    });
+
 });
